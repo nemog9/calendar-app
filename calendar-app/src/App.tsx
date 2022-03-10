7fec92793b21ddcf720836d79e9553a3bb67b5a5
nemog9 <tomone.ameno@gmail.com>
2022-03-10 12:29:39 +0000

    first commit

---
+++ calendar-app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+function App() {
+  return (
+    <FullCalendar
+      plugins={[ dayGridPlugin ]}
+      initialView="dayGridMonth"
+    />
+  );
+}
+
+export default App;
